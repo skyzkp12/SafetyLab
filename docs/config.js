a88a88b02d11ca4da87bc9c295b4edaf1143c68c
skyzkp12 <skyzkp12@gmail.com>
2026-07-23 22:46:07 +0700

    config: set live Apps Script API_URL

--- docs/config.js
+++ docs/config.js
@@ -2,6 +2,6 @@
 // แก้ API_URL ให้เป็น "Web app URL" ที่ได้ตอน Deploy Apps Script
 window.CFG = {
   // เช่น 'https://script.google.com/macros/s/AKfycb..../exec'
-  API_URL: 'PASTE_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwKFQlRYIOOODN7s5kDC-v6Cd6RLRKQP9DJM5Q4LV8w3qd157_pXz_G8TWD14OVwPpT/exec',
   APP_TITLE: 'กำหนดการ · E-Safety Lab',
 };
